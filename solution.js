9c2689f05ee30f430f3b1e285adca1cafb4dac51
reynalynbusa <reynalynbusa00@gmail.com>
2026-03-16 18:25:49 -0700

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,13 @@
+// Problem 5: The Private Inventory
+class Item {
+  #discount = 0.1;
 
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  get finalPrice() {
+    return this.price * (1 - this.#discount);
+  }
+}
