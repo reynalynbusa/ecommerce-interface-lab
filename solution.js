c4f6a1316390c64982761a026245540f33f78e64
reynalynbusa <reynalynbusa00@gmail.com>
2026-03-16 17:34:16 -0700

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,10 @@
+// Problem 3: The Functional Sum
+
+function calculateTotal(...numbers) {
+  return numbers.reduce((acc, curr) => {
+    if (typeof curr !== "number") {
+      throw new TypeError("Invalid input: All arguments must be numbers");
+    }
+    return acc + curr;
+  }, 0);
+}
