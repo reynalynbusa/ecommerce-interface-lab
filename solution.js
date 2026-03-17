f66fa6a6a35644df76ebf0c9834367bee96d7376
reynalynbusa <reynalynbusa00@gmail.com>
2026-03-16 17:48:00 -0700

    Update solution.js

---
+++ solution.js
@@ -0,0 +1,11 @@
+// Problem 2: Secure ID Generator
+function generateIDs(count) {
+  const ids = [];
+  for (let i = 0; i < count; i++) {
+    if (i === 5) {
+      continue;
+    }
+    ids.push(ID-${i});
+  }
+  return ids;
+}
